b9346c18e146fdeadc9195343114b04b49d85142
Abdulahi18 <abib05001@utb.vaxjo.se>
2023-10-26 10:16:05 +0200

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Detta är ett omvandlingsprogram");
+
+string val = "";
+
+while (val != "3")
+{
+    Console.WriteLine();
+    Console.WriteLine("Välj ett av följande alternativ");
+    Console.WriteLine("1. Omvandla meter till kilometer");
+    Console.WriteLine("2. Omvandla kilometer till meter");
+    Console.WriteLine("3. Avsluta programmet");
+    val = Console.ReadLine();
+
+    switch (val)
+    {
+        case "1":
+            Console.WriteLine("Skriv in en längd i meter");
+            double antalMeter = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Längden du skrev in är detsamma som {antalMeter / 1000} km");
+            break;
+
+        case "2":
+            Console.WriteLine("Skriv in en längd i km");
+            double antalKm = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Längden du skrev in är detsamma som {antalKm * 1000} meter");
+            break;
+
+        case "3":
+            Console.WriteLine("Programmet avslutas...");
+            break;
+
+        default:
+            Console.WriteLine("Du valde inte ett giltigt alternativ!");
+            break;
+    }
+}
